b193f773c2ab8d7c354b1acbf4609191be29e241
erenkucuk <erenkkucuk89@gmail.com>
2022-06-17 10:14:26 -1000

    project is already in remote repo,commiting more changes

--- src/Hello.java
+++ src/Hello.java
@@ -2,4 +2,7 @@ public class Hello {
     //more stuff
     //WIndows and Mac users are happy now
     //Does it work now ?
+
+    // I have already push some codes into GitHub.com
+    //Now I am ready to make more commits
 }
